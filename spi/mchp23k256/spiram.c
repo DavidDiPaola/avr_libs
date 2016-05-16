675e7ff43e47a66510436914beccfbb09934a52b
David DiPaola <DavidDiPaola@users.noreply.github.com>
2016-05-16 19:33:15 -0400

    Minor cleanup of SPI code.

--- spi/mchp23k256/spiram.c
+++ spi/mchp23k256/spiram.c
@@ -1,28 +1,6 @@
-/***
-Copyright (C) 2011 David DiPaola
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-***/
-
 #include <avr/io.h>
-#include "spi.h"
-#include "spiram.h"
+#include <avr-libs/spi/spi.h>
+#include <avr-libs/spi/mchp23k256/spiram.h>
 
 //NOTE: the CS pin on the 23K256 must be brought low before every function
 //  call and subsequently brought high after every funcion call. I don't do
